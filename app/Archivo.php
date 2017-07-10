cf965b517dc369641a625a37e8c87313e9cd8d6d
jesus <rojasd606@gmail.com>
2017-07-10 11:44:21 -0500

    relaciones

--- app/Archivo.php
+++ app/Archivo.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Archivo extends Model
 {
-    //
+
+    public function preguntas(){
+        return $this->hasMany('Pregunta');
+    }
+
+
 }
--- app/Pregunta.php
+++ app/Pregunta.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pregunta extends Model
 {
-    //
+    /*
+    * Obtiene el archivo al cual pertenece la pregunta
+    */
+    public function archivo(){
+        return $this->belongsTo('Archivo');
+    }
+
 }
